f8de01e8c564fb9fd55d9a49140469e0e153b33e
maxaiette1129 <maxaiettec@gmail.com>
2023-05-01 01:37:46 +0800

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] singleArray = new int[] { 1, 2, 3 , 4, 5};
+            for(int i = 0; i < singleArray.Length; i++) 
+            {
+                if (singleArray.Length == 5)
+                {
+                    Console.WriteLine(singleArray[i]);
+                    break;
+                }   
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            Console.Clear();
+            int[] isTemporray = new int[singleArray.Length];
+            Array.Copy(singleArray,isTemporray,singleArray.Length);
+            Console.WriteLine(isTemporray[2]);
+
+            singleArray = new int[100];
+            Array.Copy(isTemporray, singleArray, isTemporray.Length);
+        }
+    }
+}
